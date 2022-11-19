793a9d7a0838650c86e47a76256ff815301b5925
Pavel1G <94784915+Pavel1G@users.noreply.github.com>
2022-11-19 23:04:31 +0300

    Intitial commit

---
+++ src/main/java/com/example/bucket/BucketSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.bucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BucketSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BucketSpringApplication.class, args);
+    }
+
+}
