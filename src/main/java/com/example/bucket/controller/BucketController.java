0b373e7cd75eb37709a93a2e37b3cb508ac96c50
Pavel1G <94784915+Pavel1G@users.noreply.github.com>
2022-11-22 22:30:05 +0300

    Final correct

--- src/main/java/com/example/bucket/controller/BucketController.java
+++ src/main/java/com/example/bucket/controller/BucketController.java
@@ -1,5 +1,6 @@
 package com.example.bucket.controller;
 
+import com.example.bucket.model.Item;
 import com.example.bucket.service.BucketService;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,13 +15,13 @@ public class BucketController {
         this.bucketService = bucketService;
     }
 
-    @GetMapping("/store/order/add")
+    @GetMapping("/order/add")
     public ArrayList<Integer> creatItem(@RequestParam("id") List<Integer> id) {
         return this.bucketService.addItem(id);
     }
 
-    @GetMapping("/store/order/get")
-    public void getAllItem() {
-        bucketService.getAllItems();
+    @GetMapping("/order/get")
+    public List<Integer> getAllItem() {
+        return bucketService.getAllItems();
     }
 }
--- src/main/java/com/example/bucket/service/BucketService.java
+++ src/main/java/com/example/bucket/service/BucketService.java
@@ -1,12 +1,14 @@
 package com.example.bucket.service;
 
 import com.example.bucket.model.Item;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
 import java.util.*;
 
 @Service
+//@Scope("session")
 //Session — является веб-скоупом. Новый объект создается на время существование сессии
 // и закрепляется за конкретным браузером. Все запросы с определенного браузера в течение определенного
 // промежутка времени будут работать с одним объектом.
@@ -14,14 +16,12 @@ import java.util.*;
 public class BucketService {
     public ArrayList<Integer> buckets = new ArrayList<>();
 
-    public void getAllItems() {
-        this.buckets.stream()
-                .forEach(System.out::println);
+    public List<Integer> getAllItems() {
+        return this.buckets;
     }
 
 
     public ArrayList<Integer> addItem(List<Integer> id) {
-        List<Item> listItem = new LinkedList<>();
         for(Integer integer: id){
             Item item = new Item(integer);
             this.buckets.add(item.getId());
